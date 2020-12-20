660ebe92de29603b3bbec5b240d1d4c5d55958a5
Norman Meßtorff <normes@normes.org>
2020-12-20 22:22:53 +0100

    hello foo #2

--- hello-world-nginx/start.sh
+++ hello-world-nginx/start.sh
@@ -1,4 +1,6 @@
-#/bin/sh
+#!/bin/sh
 
-echo "foo: $foo, hallo: $hallo" >> /usr/share/nginx/html/index.html
+echo "foo: ${foo}, hallo: ${hallo}" >> /usr/share/nginx/html/index.html
+date >> /usr/share/nginx/html/index.html
+env >> /usr/share/nginx/html/index.html
 nginx -g "daemon off;"
